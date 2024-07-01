d8176cd468b0e17a6880ecdbbc8140d10c2c81d4
waterminer <420773173@qq.com>
2024-07-01 20:07:57 +0800

    重试、崩溃记录功能

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
-import asyncio,toml,itertools,tqdm,random
+import asyncio,toml,itertools,tqdm,random,json,time,httpx
 from novelai import NAIClient,Metadata,Model,Action,Resolution,Sampler
+from novelai.exceptions import ConcurrentError
+from os import path,remove
 
 with open("./config.toml",'r',encoding="utf-8") as f:
     config = toml.load(f)
+
+resume_list=None
+if path.exists("./crash_dump"):
+    with open("./crash_dump") as f:
+        resume_list = json.load(f)
+    remove("./crash_dump")
 
 username = config['account']['username']
 password = config['account']['password']
@@ -83,7 +91,7 @@ def metadata_builder(prompt:str,negative_prompt:str)->Metadata:
     try:
         sema = option["SMEA"]
     except:
-        sema = True
+        sema = False
     try:
         dyn = option["DYN"]
     except:
@@ -91,7 +99,7 @@ def metadata_builder(prompt:str,negative_prompt:str)->Metadata:
     try:
         scale = option["cfg_scale"]
     except:
-        scale = 5
+        scale = 6
     try: 
         cfg_rescale = option["cfg_rescale"]
     except:
@@ -122,13 +130,34 @@ async def main():
         await client.init(timeout=1000)
         return client
 
-    async def gen(metadata:Metadata):
-        print(f"prompt:{metadata.prompt}\nEstimated Anlas cost: {metadata.calculate_cost(is_opus=False)}")
-        output = await client.generate_image(
-            metadata, verbose=False, is_opus=False
-        )
-        for image in output:
-            image.save("./output",f"{prompt}.png")
+    async def gen(metadata:Metadata,retry:int=3):
+        # print(f"prompt:{metadata.prompt}\nEstimated Anlas cost: {metadata.calculate_cost(is_opus=False)}")
+        while retry >= 0:
+            try:
+                output = await client.generate_image(
+                    metadata, verbose=False, is_opus=False
+                )
+                for image in output:
+                    image.save("./output",f"{prompt}.png")
+                return
+            except httpx.RemoteProtocolError as e:
+                if retry > 0:
+                    print("failed retry in:"+str(retry))
+                    retry -= 1
+                    time.sleep(1)
+                else:
+                    raise e
+            except httpx.ConnectError as e:
+                if retry > 0:
+                    print("failed retry in:"+str(retry))
+                    retry -= 1
+                    time.sleep(1)
+                else:
+                    raise e
+            except ConcurrentError as e:
+                time.sleep(10)
+        raise Exception("Unknown Exception")
+
     client = await init()
     try:
         seed = config["option"]["seed"]
@@ -139,11 +168,21 @@ async def main():
     const_negative_prompt = config['prompt']['const_negative_prompt']
     min = config['prompt']['min_length']
     max = config['prompt']['max_length']
-    list = prompt_combinations(prompt_list,const_positive_prompt,min,max)
-    for prompt in tqdm.tqdm(list):
-        metadata=metadata_builder(prompt,const_negative_prompt)
-        if seed == 0:
-            metadata.seed=random.randint(1,999999999)
-        await gen(metadata)
+    if resume_list:
+        print("检测到意外中断，会将从上次任务断点继续")
+        list = resume_list
+    else:
+        list = prompt_combinations(prompt_list,const_positive_prompt,min,max)
+    try:
+        for index,prompt in enumerate(tqdm.tqdm(list)):
+            metadata=metadata_builder(prompt,const_negative_prompt)
+            if seed == 0:
+                metadata.seed=random.randint(1,999999999)
+            await gen(metadata)
+    except Exception as e:
+        crash = list[index:]
+        with open("crash_dump","w",encoding="utf8") as f:
+            json.dump(crash,f)
+        raise e
 
 asyncio.run(main())
